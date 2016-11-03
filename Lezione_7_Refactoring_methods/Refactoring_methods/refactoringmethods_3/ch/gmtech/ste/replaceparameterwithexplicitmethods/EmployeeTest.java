ddc6e90d349dd30a9b9887885cab7c697123ace3
kolch182 <stefanocoluccia@hotmail.com>
2016-11-03 14:28:08 +0100

    Lezione 7 - sistemati alcuni problemi

--- Lezione_7_Refactoring_methods/Refactoring_methods/refactoringmethods_3/ch/gmtech/ste/replaceparameterwithexplicitmethods/EmployeeTest.java
+++ Lezione_7_Refactoring_methods/Refactoring_methods/refactoringmethods_3/ch/gmtech/ste/replaceparameterwithexplicitmethods/EmployeeTest.java
@@ -1,6 +1,6 @@
 package ch.gmtech.ste.replaceparameterwithexplicitmethods;
 
-import static junit.framework.Assert.*;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
--- Lezione_7_Refactoring_methods/Refactoring_methods/refactoringmethods_3/ch/gmtech/ste/replaceparameterwithexplicitmethods/EmployeeType.java
+++ Lezione_7_Refactoring_methods/Refactoring_methods/refactoringmethods_3/ch/gmtech/ste/replaceparameterwithexplicitmethods/EmployeeType.java
@@ -4,18 +4,4 @@ public class EmployeeType {
 	static final int ENGINEER = 0;
 	static final int SALESMAN = 1;
 	static final int MANAGER = 2;
-
-	static Employee create(int type) {
-		switch (type) {
-		case ENGINEER:
-			return Employee.createEngineer();
-		case SALESMAN:
-			return Employee.createSalesman();
-		case MANAGER:
-			return Employee.createManager();
-		default:
-			throw new IllegalArgumentException("Incorrect type code value");
-		}
-	}
-
 }
