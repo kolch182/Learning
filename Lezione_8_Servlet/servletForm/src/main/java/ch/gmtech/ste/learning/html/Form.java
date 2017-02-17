924d243e7021ff121e4c3aecf7ac8a38047bc97e
stefano <stefano.coluccia@mfgroup.ch>
2017-02-17 14:42:55 +0100

    Da lavoro - creato form tramite j2html, creare interfaccia x html in generale

--- Lezione_8_Servlet/servletForm/src/main/java/ch/gmtech/ste/learning/html/Form.java
+++ Lezione_8_Servlet/servletForm/src/main/java/ch/gmtech/ste/learning/html/Form.java
@@ -1,5 +1,6 @@
 package ch.gmtech.ste.learning.html;
 import static j2html.TagCreator.*;
+import j2html.tags.ContainerTag;
 
 public class Form{
 
@@ -19,26 +20,54 @@ public class Form{
 	public String renderBody(){
 
 		return body().with(
-					div().withClass("container").with(
+				div().withClass("container").with(
+					div().withClass("row").with(
 						div().withClass("col-md-6 col-md-offset-3").with(
-							h1("Course Form").withClass("page-header text-center").with(
-								form().withId("courseForm")).withClass("form-horizontal").withMethod("post").withAction("/try/me").with(
-									div().withClass("form-group").with( 
-										label("Name").withClass("col-sm-2 control-label"),
-										div().withClass("col-sm-10").with(
-											input()
-											.withType("text")
-									        .withId("name")
-									        .withName("name")
-									        .withPlaceholder(placeholder)
-										)
-									)
+							h1("Course Form").withClass("page-header text-center"),
+								form().withId("courseForm").withClass("form-horizontal").withMethod("post").withAction("/try/me").with(
+		
+									createInput("Name","text","form-control","courseName","name","Course Name"),
+									createInput("Number","number","form-control","number","number","Number"),
+									createInput("Location","text","form-control","location","location","Location"),
+									createInput("#Seats","number","form-control","seats","seats","#Seats"),
+									createInput("StartDate","date","form-control","startdate","startdate","StartDate"),
+									createSubmit(),
+									script().withSrc("/js/jquery.min.js"),
+									script().withSrc("/js/bootstrap.min.js")
 								)
 							)
 						)
 					)
+				)
 				.render()
 				;
+	}
+
+	private ContainerTag createSubmit() {
+		return div().withClass("form-group").with( 
+			div().withClass("col-sm-10 col-sm-offset-2").with(
+					input()
+					.withType("submit")
+					.withClass("btn btn-primary")
+			        .withId("submit")
+			        .withValue("Send")
+			)
+		);
+	}
+
+	private ContainerTag createInput(String value, String type, String inputClass, String id, String name, String placeHolder ) {
+		return div().withClass("form-group").with( 
+			label(value).withClass("col-sm-2 control-label"),
+			div().withClass("col-sm-10").with(
+				input()
+				.withType(type)
+				.withClass(inputClass)
+		        .withId(id)
+		        .withName(name)
+		        .withPlaceholder(placeHolder)
+		        .withValue(value)
+			)
+		);
 	}
 	
 	
